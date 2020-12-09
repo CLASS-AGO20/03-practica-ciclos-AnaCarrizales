be13db1acdd085c300eff3e01d0219eb6e35b85f
Ana Carrizales <acarrizales2@ucol.mx>
2020-12-09 16:10:17 -0600

    convertirAString() was created and tested

--- src/main.js
+++ src/main.js
@@ -10,9 +10,26 @@ export default class App {
         return resultado;
     }
 
+    convertirAString(numero)
+    {
+        let i = 1;
+        let resultado = "";
+        while(i <= numero)
+        {
+            resultado = "*" + resultado;
+            i++;
+        }
+        return resultado;
+    }
+
+
 }
 
 let app = new App();
 
 console.log("Probando factorial()");
 console.log(app.factorial(5));
+
+console.log("Probando convertirAString()");
+console.log(app.convertirAString(5));
+
