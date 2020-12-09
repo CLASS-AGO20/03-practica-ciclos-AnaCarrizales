c0507fef0285c07a3db4769794623be4b09b9dad
Ana Carrizales <acarrizales2@ucol.mx>
2020-12-09 15:52:59 -0600

    factorial() was created and tested

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
-export default class App {}
+export default class App {
+
+    factorial(numero)
+    {
+        let resultado = 1;
+        for (let i = numero; i > 0 ; i --)
+        {
+            resultado *= i;
+        }
+        return resultado;
+    }
+
+}
 
 let app = new App();
+
+console.log("Probando factorial()");
+console.log(app.factorial(5));
